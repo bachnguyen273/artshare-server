3b8b6aec1457297c628a86f099681913f15b6c7a
Nguyen Vuong Bach <nvbach273@gmail.com>
2024-03-01 13:53:02 +0700

    Add upload file

---
+++ artshare-server.Services/Interfaces/IAzureBlobStorageService.cs
@@ -0,0 +1,15 @@
+﻿using artshare_server.ApiModels.DTOs;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace artshare_server.Services.Interfaces
+{
+    public interface IAzureBlobStorageService
+    {
+        Task<string> UploadFileAsync(string containerName, IFormFile file);
+    }
+}
