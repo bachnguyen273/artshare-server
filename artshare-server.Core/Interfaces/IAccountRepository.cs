a4cacd5a232f8278acca81abc9d9e4b6bf1913d5
Minh Phuc <99303499+MPFU@users.noreply.github.com>
2024-03-03 14:22:27 +0700

    Phucnvm (#3)
    * Get profile by id, username, update profile basic
    
    Signed-off-by: MinhPhuc <phucnvm2210@gmail.com>
    
    * add interface find by username
    
    Signed-off-by: MinhPhuc <phucnvm2210@gmail.com>
    
    * get all account vs fix update
    
    Signed-off-by: MinhPhuc <phucnvm2210@gmail.com>
    
    * add authorize
    
    Signed-off-by: MinhPhuc <phucnvm2210@gmail.com>
    
    * Fix conflict
    
    Signed-off-by: MinhPhuc <phucnvm2210@gmail.com>
    
    * Change dbcontext name conflict
    
    Signed-off-by: MinhPhuc <phucnvm2210@gmail.com>
    
    ---------
    
    Signed-off-by: MinhPhuc <phucnvm2210@gmail.com>

--- artshare-server.Core/Interfaces/IAccountRepository.cs
+++ artshare-server.Core/Interfaces/IAccountRepository.cs
@@ -1,9 +1,11 @@
-﻿using artshare_server.Core.Models;
+﻿
+using artshare_server.Core.Models;
 
 namespace artshare_server.Core.Interfaces
 {
     public interface IAccountRepository : IGenericRepository<Account>
     {
         Task<Account?> GetByEmailAsync(string email);
+        Task<Account?> GetByUsernameAsync(string username);
     }
 }
