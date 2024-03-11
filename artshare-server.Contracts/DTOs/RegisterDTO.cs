142c71750557e8362bccd99c02c397121edff61d
Nguyen Vuong Bach <nvbach273@gmail.com>
2024-03-11 21:47:32 +0700

    Add register and combine image

--- artshare-server.Contracts/DTOs/RegisterDTO.cs
+++ artshare-server.Contracts/DTOs/RegisterDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,7 @@ namespace artshare_server.Contracts.DTOs
     {
         public string Email { get; set; }
         public string Password { get; set; }
-        public string UserName { get; set; }
+		public string UserName { get; set; }
         public string FullName { get; set; }
         public string PhoneNumber { get; set; }
         public string Role { get; set; }
