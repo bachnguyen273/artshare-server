f3f0dcc81adb2e310066fec822c28bdb0227c09d
Nguyen Vuong Bach <nvbach273@gmail.com>
2024-03-14 15:49:46 +0700

    Update

--- artshare-server.WebApp/Pages/Artworks.cshtml.cs
+++ artshare-server.WebApp/Pages/Artworks.cshtml.cs
@@ -1,4 +1,6 @@
+using artshare_server.Core.Enums;
 using artshare_server.Core.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Newtonsoft.Json;
@@ -10,8 +12,9 @@ namespace artshare_server.WebApp.Pages
     public class ArtworksModel : PageModel
     {
 		public dynamic Artwork { get; set; }
+        public string Role { get; set; }
 
-		public async Task<IActionResult> OnGetAsync(int artworkId)
+        public async Task<IActionResult> OnGetAsync(int artworkId)
 		{
             IConfiguration config = new ConfigurationBuilder()
                .SetBasePath(Directory.GetCurrentDirectory())
@@ -31,5 +34,48 @@ namespace artshare_server.WebApp.Pages
                 return Page();
             }
         }
-	}
+
+        public IActionResult OnPostAddToCart(int itemId)
+        {
+            Role = HttpContext.Session.GetString("AccountRole");
+            if (AccountRole == "Customer")
+            {
+                try
+                {
+                    var cage = _birdCageRepository.GetCageById(itemId);
+
+                    if (cage != null)
+                    {
+                        var cart = HttpContext.Session.Get<Dictionary<BirdCage, int>>("Cart");
+                        if (cart != null)
+                        {
+                            if (cart.ContainsKey(cage))
+                            {
+                                cart[cage]++;
+                            }
+                            else
+                            {
+                                cart.Add(cage, 1);
+                            }
+                            HttpContext.Session.Set("Cart", cart);
+                        }
+                        else
+                        {
+                            cart = new Dictionary<BirdCage, int> { { cage, 1 } };
+                            HttpContext.Session.Set("Cart", cart);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    TempData["ErrorMessage"] = "An error occurred: " + ex.Message;
+                }
+                return RedirectToPage();
+            }
+            else
+            {
+                return RedirectToPage("/Users/Login");
+            }
+        }
+    }
 }
