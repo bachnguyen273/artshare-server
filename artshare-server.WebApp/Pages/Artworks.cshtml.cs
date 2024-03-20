1de9c3717511192d5d9115ca2cdc3770bb62d68d
PhatBaka <nguyenquyphat2711@gmail.com>
2024-03-20 19:56:37 +0700

    fix

--- artshare-server.WebApp/Pages/Artworks.cshtml.cs
+++ artshare-server.WebApp/Pages/Artworks.cshtml.cs
@@ -12,25 +12,30 @@ namespace artshare_server.WebApp.Pages
 		public dynamic Artwork { get; set; }
         public string Role { get; set; }
 
-        public async Task<IActionResult> OnGetAsync(int artworkId)
-		{
-            IConfiguration config = new ConfigurationBuilder()
-               .SetBasePath(Directory.GetCurrentDirectory())
-               .AddJsonFile("appsettings.json", true, true)
-               .Build();
+  //      public async Task<IActionResult> OnGetAsync(int artworkId)
+		//{
+  //          //IConfiguration config = new ConfigurationBuilder()
+  //          //   .SetBasePath(Directory.GetCurrentDirectory())
+  //          //   .AddJsonFile("appsettings.json", true, true)
+  //          //   .Build();
 
-            string apiUrl = config["API_URL"];
-            using var client = new HttpClient();
-            using (var httpClient = new HttpClient())
-            {
-                HttpResponseMessage artworkResponseMessage = await httpClient.GetAsync(apiUrl + "/Artwork/GetArtworkById?id=" + artworkId);
-                artworkResponseMessage.EnsureSuccessStatusCode();
-                string artworkContent = await artworkResponseMessage.Content.ReadAsStringAsync();
-                dynamic artworkObject = JsonConvert.DeserializeObject(artworkContent);
-                Artwork = artworkObject.data.artwork;
+  //          //string apiUrl = config["API_URL"];
+  //          //using var client = new HttpClient();
+  //          //using (var httpClient = new HttpClient())
+  //          //{
+  //          //    HttpResponseMessage artworkResponseMessage = await httpClient.GetAsync(apiUrl + "/Artwork/GetArtworkById?id=" + artworkId);
+  //          //    artworkResponseMessage.EnsureSuccessStatusCode();
+  //          //    string artworkContent = await artworkResponseMessage.Content.ReadAsStringAsync();
+  //          //    dynamic artworkObject = JsonConvert.DeserializeObject(artworkContent);
+  //          //    Artwork = artworkObject.data.artwork;
 
-                return Page();
-            }
-        }     
+  //          //    return Page();
+  //          //}
+  //      }     
+
+        public void OnGet()
+        {
+
+        }
     }
 }
