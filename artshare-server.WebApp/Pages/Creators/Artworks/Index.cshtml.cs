92ca71dcdd0988e05ee54d61949b288b8e4cf2dd
Nguyen Vuong Bach <nvbach273@gmail.com>
2024-03-21 22:55:46 +0700

    update

--- artshare-server.WebApp/Pages/Creators/Artworks/Index.cshtml.cs
+++ artshare-server.WebApp/Pages/Creators/Artworks/Index.cshtml.cs
@@ -1,5 +1,7 @@
+using artshare_server.WebApp.Helpers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.IdentityModel.Tokens;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.IdentityModel.Tokens.Jwt;
@@ -10,8 +12,13 @@ namespace artshare_server.WebApp.Pages.Creators.Artworks
 {
     public class IndexModel : PageModel
     {
-        public List<dynamic> Artworks { get; set; }
+        public IEnumerable<dynamic> Artworks { get; set; }
         public List<dynamic> Genres { get; set; }
+        public Pager Pager { get; set; }
+        public int? GenreID { get; set; }
+        public string Search { get; set; }
+
+        private const int pageSize = 5;
         private readonly HttpClient _httpClient;
         private readonly string _apiURL;
 
@@ -31,6 +38,14 @@ namespace artshare_server.WebApp.Pages.Creators.Artworks
         public async Task<IActionResult> OnPostAsync(int? id = null)
         {
             await LoadData(id);
+            GenreID = id;
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostSearch()
+        {
+            Search = Request.Form["SearchValue"];
+            await LoadData();
             return Page();
         }
 
@@ -48,11 +63,40 @@ namespace artshare_server.WebApp.Pages.Creators.Artworks
             {
                 artworkUrl += $"&GenreId={id}";
             }
+            else if (!HttpContext.Request.Query["genre"].IsNullOrEmpty())
+            {
+                artworkUrl += $"&GenreId={HttpContext.Request.Query["genre"]}";
+            }
+            else if (!Search.IsNullOrEmpty())
+            {
+                artworkUrl += $"&Title={Search}";
+            }
+            else if (!HttpContext.Request.Query["searchVal"].IsNullOrEmpty())
+            {
+                artworkUrl += $"&Title={HttpContext.Request.Query["searchVal"]}";
+            }
             HttpResponseMessage artworkResponseMessage = await _httpClient.GetAsync(artworkUrl);
             artworkResponseMessage.EnsureSuccessStatusCode();
             string artworkContent = await artworkResponseMessage.Content.ReadAsStringAsync();
             dynamic artworkObject = JsonConvert.DeserializeObject(artworkContent);
-            Artworks = new List<dynamic>(artworkObject.data.artwork.items);
+            IEnumerable<dynamic> listArtwork = new List<dynamic>(artworkObject.data.artwork);
+            //paging artwork
+            int page;
+            if (HttpContext.Request.Query["page"].IsNullOrEmpty())
+            {
+                page = 1;
+            }
+            else
+            {
+                page = int.Parse(HttpContext.Request.Query["page"]);
+            }
+            if (page < 1)
+            {
+                page = 1;
+            }
+            int recs = listArtwork.Count();
+            Pager = new Pager(recs, page, pageSize);
+            Artworks = listArtwork.Skip((page - 1) * pageSize).Take(Pager.PageSize);
 
             string genreUrl = $"{_apiURL}/Genre/GetGenres";
             HttpResponseMessage genreResponseMessage = await _httpClient.GetAsync(genreUrl);
